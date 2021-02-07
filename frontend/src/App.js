c6cef41a32c42eb37a42fa85046219a4f501bdee
Sagar <sagarpatkar17@gmail.com>
2021-02-07 12:00:24 +0530

    Implement React Router in App.js & Product.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,22 @@ import React from "react";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen.jsx";
+import ProductScreen from "./screens/ProductScreen.jsx";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
