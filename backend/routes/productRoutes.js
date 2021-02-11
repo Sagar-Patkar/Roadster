da135aea4bdac3fb307ea0abfc37907730e992aa
Sagar <sagarpatkar17@gmail.com>
2021-02-11 13:58:40 +0530

    productController file, update productRoutes file

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,33 +1,11 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
 const router = express.Router();
-import Product from "../models/productModel.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 
-// @desc:  Fetch all Products
-// @route: GET /api/products
-// @access: PUBLIC
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-// @desc:  Fetch Products by Id
-// @route: GET /api/products/:id
-// @access: PUBLIC
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product Not Found");
-    }
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
